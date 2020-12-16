1daff74d9760e1bf177db49f585d6a20415fd1a5
nobumacsuzuki <nobumac.suzuki@gmail.com>
2020-12-15 18:04:53 -0800

    update

--- PillowTest.py
+++ PillowTest.py
@@ -4,11 +4,10 @@ def main():
     filename = 'lena_512x512.bmp'
     imageFromFile = Image.open(filename)
     imageBuffer = Image.new('RGB', imageFromFile.size)
-    for x_pos in range(imageFromFile.size[0]):
-        for y_pos in range(imageFromFile.size[1]):
+    for y_pos in range(imageFromFile.size[1]):
+        for x_pos in range(imageFromFile.size[0]):
             (inR, inG, inB) = imageFromFile.getpixel((x_pos, y_pos))
-            Y = inR * 3 +  inG * 6 + inB
-            Y = int(Y / 10)
+            Y = int(inR * .3 +  inG * .6 + inB*.1)
             outR = Y
             outG = Y
             outB = Y
